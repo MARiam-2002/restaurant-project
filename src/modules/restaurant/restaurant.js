ae0d5917e742bda6bd800fac79aac6b1ac50b522
mariam khaled <khaldmrym769@gmail.com>
2023-12-26 14:32:58 +0200

    updated

--- src/modules/restaurant/restaurant.js
+++ src/modules/restaurant/restaurant.js
@@ -21,15 +21,16 @@ export const createPopularItems = asyncHandler(async (req, res, next) => {
       image: { id: public_id, url: secure_url },
     });
   }
-  const { secure_url, public_id } = await cloudinary.uploader.upload(
-    req.files.logo[0].path,
-    {
-      folder: `${process.env.FOLDER_CLOUDINARY}/restaurants/logo`,
-    }
-  );
-
-  createPopularRestaurantItems.logo = { id: public_id, url: secure_url };
-  await createPopularRestaurantItems.save();
+  if (req.files.logo) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      req.files.logo[0].path,
+      {
+        folder: `${process.env.FOLDER_CLOUDINARY}/restaurants/logo`,
+      }
+    );
+    createPopularRestaurantItems.logo = { id: public_id, url: secure_url };
+    await createPopularRestaurantItems.save();
+  }
   return res.json({ success: true, result: createPopularRestaurantItems });
 });
 
@@ -75,24 +76,21 @@ export const updateRestaurant = asyncHandler(async (req, res, next) => {
   return res.json({ message: "updated", result: newPopularRestaurantItems });
 });
 
-
 export const deleteRestaurant = asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
-    const restaurant = await restaurantModel.findById({ _id: id });
-    if (!restaurant) {
-      return res.json({ message: "invalid-restaurant" });
-    }
-  
-    const ids = [restaurant.image.id,restaurant.logo.id];
-    const result = await cloudinary.api.delete_resources(ids);
-   
-  
-    const deleteRestaurant = await restaurantModel.findByIdAndDelete({ _id: id });
-    return res.json({ message: "deleted", result: deleteRestaurant });
-  });
+  const { id } = req.params;
+  const restaurant = await restaurantModel.findById({ _id: id });
+  if (!restaurant) {
+    return res.json({ message: "invalid-restaurant" });
+  }
 
+  const ids = [restaurant.image.id, restaurant.logo.id];
+  const result = await cloudinary.api.delete_resources(ids);
 
-  export const getRestaurants = asyncHandler(async (req, res, next) => {
-    const restaurant = await restaurantModel.find({});
-    res.json({ success: true, result: restaurant });
-  });
+  const deleteRestaurant = await restaurantModel.findByIdAndDelete({ _id: id });
+  return res.json({ message: "deleted", result: deleteRestaurant });
+});
+
+export const getRestaurants = asyncHandler(async (req, res, next) => {
+  const restaurant = await restaurantModel.find({});
+  res.json({ success: true, result: restaurant });
+});
