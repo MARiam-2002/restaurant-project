70f6708b6d8d26993b061beb8b80eaffd05199ac
mariam khaled <khaldmrym769@gmail.com>
2023-12-27 10:30:43 +0200

    updated

--- src/modules/bestFood/bestFood.js
+++ src/modules/bestFood/bestFood.js
@@ -1,26 +1,25 @@
-import bestFoodModel from "../../../DB/model/bestFood.model.js";
 import cloudinary from "../../utils/cloudinary.js";
 import { asyncHandler } from "../../utils/errorHandling.js";
-
+import bestFoodModel from '../../../DB/model/bestFood.model.js'
 export const createBestFood = asyncHandler(async (req, res, next) => {
   const { title, description } = req.body;
 
-  const createBestFood = await bestFoodModel.create({
+  let createBestFoods = await bestFoodModel.create({
     title,
     description,
   });
 
-  if (req.file.image) {
+  if (req.file) {
     const { secure_url, public_id } = await cloudinary.uploader.upload(
       req.file.path,
       {
         folder: `${process.env.FOLDER_CLOUDINARY}/bestFood`,
       }
     );
-    createBestFood.image = { id: public_id, url: secure_url };
-    await createBestFood.save();
+    createBestFoods.image = { id: public_id, url: secure_url };
+    await createBestFoods.save();
   }
-  return res.json({ success: true, result: createBestFood });
+  return res.json({ success: true, result: createBestFoods});
 });
 
 export const getBestFood = asyncHandler(async (req, res, next) => {
